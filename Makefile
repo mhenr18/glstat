bb77cc23edbb67c959e9b6999d1be495cc52e1ed
Matthew Henry <matt.henry@me.com>
2014-04-14 07:48:18 +1000

    Simplified payload and moved it back to C - payload no longer converts into FPS

--- Makefile
+++ Makefile
@@ -1,21 +1,21 @@
 CC = clang
-CXX = clang++ -std=c++11 --stdlib=libc++
 
-PAYLOAD_SRCS = glstatPayload.cc
+PAYLOAD_SRCS = glstatPayload.c
+PAYLOAD_OBJS = $(subst .c,.o,$(PAYLOAD_SRCS))
 
 MACH_OVERRIDE_SRCS = $(shell ls mach_override/mach_override.c mach_override/libudis86/*.c)
 MACH_OVERRIDE_OBJS = $(subst .c,.o,$(MACH_OVERRIDE_SRCS))
 
 all: glstat.dylib
 
-glstat.dylib: $(MACH_OVERRIDE_OBJS) $(PAYLOAD_SRCS)
-	$(CXX) -arch i386 -arch x86_64 -g -framework OpenGL -framework CoreServices -dynamiclib $(PAYLOAD_SRCS) $(MACH_OVERRIDE_OBJS) -o $@
+glstat.dylib: $(MACH_OVERRIDE_OBJS) $(PAYLOAD_OBJS)
+	$(CC) -arch i386 -arch x86_64 -g -framework OpenGL -framework CoreServices -dynamiclib $(PAYLOAD_OBJS) $(MACH_OVERRIDE_OBJS) -o $@
 
 %.o: %.c
 	$(CC) -c -arch i386 -arch x86_64 $< -o $@
 
 clean:
-	rm -f $(MACH_OVERRIDE_OBJS)
+	rm -f $(PAYLOAD_OBJS) $(MACH_OVERRIDE_OBJS)
 	rm -f glstat.dylib
 	rm -f glstat_fmt
 	rm -rf glstat.dylib.dSYM glstat_fmt.dSYM
---
+++ glstatPayload.c
@@ -0,0 +1,55 @@
+//
+// glstatPayload.c
+// Copyright (c) 2014 Matthew Henry.
+// MIT licensed - refer to LICENSE.txt for details.
+//
+
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#include <OpenGL/OpenGL.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "mach_override/mach_override.h"
+
+FILE *fin, *fout, *ferr;
+CGLError (*CGLFlushDrawableReentry)(CGLContextObj);
+mach_timebase_info_data_t timebaseInfo;
+uint64_t startTime;
+
+double getTime()
+{
+    return ((mach_absolute_time() - startTime) * timebaseInfo.numer) / 
+        (double)timebaseInfo.denom / 1e9;
+}
+
+CGLError CGLFlushDrawableOverride(CGLContextObj ctx)
+{
+    fprintf(fout, "%p, CGLFlushDrawable, %.6f\n", ctx, getTime());
+    
+    return CGLFlushDrawableReentry(ctx);
+}
+
+void payload_main(int in, int out, int err)
+{
+    fin = fdopen(in, "r");
+    fout = fdopen(out, "w");
+    ferr = fdopen(out, "w");
+
+    setvbuf(fin, NULL, _IONBF, 0);
+    setvbuf(fout, NULL, _IONBF, 0);
+    setvbuf(ferr, NULL, _IONBF, 0);
+
+    mach_timebase_info(&timebaseInfo);
+    startTime = mach_absolute_time();
+
+    fprintf(fout, "context, function, call time\n");
+
+    if (mach_override_ptr((void *)CGLFlushDrawable, 
+        (void *)CGLFlushDrawableOverride, (void **)&CGLFlushDrawableReentry))
+    {
+        fprintf(ferr, "glstat: failed to override CGLFlushDrawable\n");
+        return;
+    }
+}
